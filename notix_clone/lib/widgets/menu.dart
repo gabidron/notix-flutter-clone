ec1a772d8b3ca1b89267c3971344b9367b42ea31
Dron Ionut-Gabriel <drongabi8@gmail.com>
2022-02-10 11:11:50 +0200

    menu items

--- notix_clone/lib/widgets/menu.dart
+++ notix_clone/lib/widgets/menu.dart
@@ -6,13 +6,45 @@ class Menu extends StatelessWidget implements PreferredSizeWidget {
   @override
   Size get preferredSize => const Size.fromHeight(75);
 
+  final List<String> _navItems = const [
+    "Solutions",
+    "Pricing",
+    "Partners",
+    "Features",
+    "Case Studies",
+    "Get a demo",
+    "Blog",
+  ];
+  final List<String> _options = const [];
+
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Container(),
-        Container(),
-        Container(),
+        Container(
+          child: Image.asset('assets/images/logo.png'),
+        ),
+        Container(
+            child: Row(
+          children: _navItems.map((e) => Text(e)).toList(),
+        )),
+        Container(
+          child: Row(
+            children: [
+              const Text(
+                "Login",
+              ),
+              const Text(
+                "Ru",
+              ),
+              ElevatedButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "Sign up",
+                  ))
+            ],
+          ),
+        ),
       ],
     );
   }
